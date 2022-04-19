baad116dd104a5e81ae9cea821572e6e5cd24019
Sandro927 <47231162+Sandro927@users.noreply.github.com>
2022-04-19 01:53:48 -0400

    styled login page

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function login() {
   return (
-    <div>
-        <h1>This is the login page!</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+        <img src="https://links.papareact.com/9xl" className="w-52 mb-5" alt="Spotify logo"/>
+        <div>
+            <button className="bg-[#18D860] text-white p-5 rounded-full">Login with Spotify</button>
+        </div>
     </div>
   )
 }
