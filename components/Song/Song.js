02728e11838abdf623ea7f9af76995716bd37518
Sandro927 <47231162+Sandro927@users.noreply.github.com>
2022-04-20 01:09:59 -0400

    created Song component to display song info

--- components/Song/Song.js
+++ components/Song/Song.js
@@ -4,14 +4,24 @@ import useSpotify from '../../hooks/useSpotify';
 function Song({order, track}) {
     const spotifyAPI = useSpotify();
     return (
-    <div>
-        <div>
+    <div className="grid grid-cols-2">
+        <div className="flex items-center space-x-4">
             <p>{order + 1}</p>
             <img 
                 src={track.track.album.images[0].url}
                 alt="Song Avatar"
                 className="h-10 w-10"
             />
+            
+            <div>
+                <p>{track.track.name}</p>
+                <p>{track.track.artists[0].name}</p>
+            </div>
+        </div>
+
+        <div>
+            <p>{track.track.album.name}</p>
+            <p>duration</p>
         </div>
     </div>
     )
