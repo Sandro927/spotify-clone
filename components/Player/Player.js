26b3aab0010da473c5569014daa2d4db342111e1
Sandro927 <mandala.alejandro@gmail.com>
2022-04-23 09:23:19 -0400

    created a custom hook to grab song info

--- components/Player/Player.js
+++ components/Player/Player.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useSession } from 'next-auth/react';
+import React from 'react';
+import { currentTrackIdState, isPlayingState } from '../../atoms/songAtom';
+import useSpotify from '../../hooks/useSpotify';
+import { useRecoilState } from 'recoil'
+import { useState } from 'react';
 
 function Player() {
+
+    const spotifyAPI = useSpotify();
+    const { data: session, status } = useSession(); 
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setisPlaying] = useRecoilState(isPlayingState);
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [volume, setVolume] = useState(50);
+    
+
   return (
     <div>
-        <div></div>
+        <div>
+            <img src="" alt=""/>
+        </div>
     </div>
   )
 }
