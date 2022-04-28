bbf3ea1277b24bba1d39948c7ef92eb65ec0a7aa
Sandro927 <47231162+Sandro927@users.noreply.github.com>
2022-04-27 22:21:24 -0400

    added debounce to volume to prevent API spamming

--- components/Player/Player.js
+++ components/Player/Player.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { currentTrackIdState, isPlayingState } from '../../atoms/songAtom';
 import useSpotify from '../../hooks/useSpotify';
 import { useRecoilState } from 'recoil'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import useSongInfo from '../../hooks/useSongInfo';
 import { HeartIcon, VolumeUpIcon as VolumeDownIcon } from '@heroicons/react/outline';
+import { debounce } from 'lodash';
 import { 
     FastForwardIcon,
     PauseIcon,
@@ -63,6 +64,30 @@ function Player() {
             setVolume(50);
         }
     }, [currentTrackId, spotifyApi, session])
+    
+    useEffect(() => {
+       
+        if (volume > 0 && volume < 100) {
+            debouncedAdjustedVolume(volume);
+        }
+    }, [volume]);
+
+    const handleVolumeDownClick = () => {
+        volume > 0 && setVolume(volume - 10)
+    }
+
+    const handleVolumeUpClick = () => {
+        volume < 100 && setVolume(volume + 10)
+    }
+
+    const debouncedAdjustedVolume = useCallback(
+        debounce((volume) => {
+            spotifyApi.setVolume(volume)
+            .catch((err) => {
+                console.log(err)
+            });
+        }, 500, [])
+    )
 
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
@@ -96,7 +121,11 @@ function Player() {
         </div>
 
         <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-            <VolumeDownIcon className="button"/>
+            <VolumeDownIcon 
+                className="button"
+                onClick={handleVolumeDownClick}
+            />
+            
             <input 
                 className="w-14 md:w-28" 
                 type="range" 
@@ -105,7 +134,10 @@ function Player() {
                 min={0} 
                 max={100} 
             />
-            <VolumeUpIcon className="button"/>
+            <VolumeUpIcon 
+                className="button"
+                onClick={handleVolumeUpClick}
+            />
         </div>
     </div>
   )
